6341869622f0eb1fc7339529c86b148e5ef5c1eb
DauBap <ngoquangtruongxuan@gmail.com>
2022-09-17 13:38:38 +0700

    add new project

---
+++ UFO/script.js
@@ -0,0 +1,7 @@
+function Fly(event){
+    console.log(event.clientX,event.clientY);
+    var ufo =document.getElementById('ufo');
+    ufo.style.left = event.clientX+'px';
+    ufo.style.top = event.clientY + 'px';
+ 
+}
